0bb9f69f9e0a65dd9178a41383631017eb9899f7
eddvarg1 <eddvarg1@yahoo.com>
2017-12-24 12:00:27 -0600

    sql connects to existing database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,67 @@
 var express = require('express');
-var app = express();
 var router = express.Router();
-var mustache = require('mustache');
+var Mustache = require('mustache');
 var fs = require('fs');
-var path = require('path');
+var mysql= require('mysql');
+var appp= express();
+
+/*appp.listen('3000',()=>{
+  console.log('server started');
+})*/
+//initialize database with table
+var db= mysql.createConnection({
+
+  host:'localhost',
+  user: 'root',
+  password:'',
+  database: 'sampledb'
+});
+ 
+db.connect(function(error) {
+  if(error)
+  console.log('error');
+  else 
+  console.log('connected');
+});
+
+router.get('/name', function(req,res){
+ 
+  db.query("INSERT INTO temp SET NAME='kalee', PASSWORD='5846' ", function(error, rows, fields) {
+  if(error)
+  console.log('error selecting from data base');
+  else 
+  console.log('QUERY SUCCESSFUL');
+  })
+});
+
+router.get('/namee', function(req,res){
+ 
+  db.query("DELETE FROM temp WHERE NAME='kalee' ", function(error, rows, fields) {
+  if(error)
+  console.log('error selecting from data base');
+  else 
+  console.log('QUERY SUCCESSFUL');
+  })
+});
+router.get('/table', function(req,res){
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
+
+router.get('/deltable', function(req,res){
+  var sql = "DROP TABLE customers";
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table deleted");
+  });
+});
+
+
+
+
 
 
 
@@ -27,10 +85,16 @@ router.get('/', function(req, res, next) {
         Name: "Profile"
       }
     ]
+  }
+  
+  var view = {
+    /*change these to get from table*/
+    name: "Joe",
+    content: "Moose are majestic"
   };
   
 
-  var file = fs.readFile("./src/html/homepage.html", "utf-8", function(err,content) {
+  var file = fs.readFile("./src/html/index.mustache", "utf-8", function(err,content) {
     if(err) {
       console.log("Error: " + err);
       res.render('index', { title: 'Express' });
@@ -38,8 +102,8 @@ router.get('/', function(req, res, next) {
       return;
     }
     
-    console.log("");
-    var output = mustache.render(content, data);
+    console.log(content);
+    var output = Mustache.render(content, view);
     //res.render('index', { title: 'Express' });
     //res.send(output, { title: 'Mustache' });
     res.send(output);
