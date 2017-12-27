db2d18a8638efbc11acc584d0c2ded913ce75e4b
Kalee Kuhn <kaleegk26@gmail.com>
2017-12-26 18:55:15 -0600

    Integerated with bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,10 @@ document.getElementById("Events").addEventListener("click", myFunction);
 function myFunction() {
     alert ("AAAHHH!!!! Events has been clicked!?!");
 }
+
+$(document).ready(function(){
+    $('.homepageContainer').css({'height':(($(window).height())-150)+'px'});
+});
+$(window).resize(function(){
+    $('.homepageContainer').css({'height':(($(window).height())-150)+'px'});
+});
